507afbaab79e966afce5bb77a9f53c16d49b8935
DANIEL OREILLY <danieloreilly@DANIELs-MBP.fios-router.home>
2016-01-27 19:14:43 -0800

    finished

---
+++ favorite-place/DetailVC.swift
@@ -0,0 +1,39 @@
+//
+//  DetailVC.swift
+//  favorite-place
+//
+//  Created by DANIEL OREILLY on 1/27/16.
+//  Copyright © 2016 DANIEL OREILLY. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func backPressed(sender: AnyObject) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
